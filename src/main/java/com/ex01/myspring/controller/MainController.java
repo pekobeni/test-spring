dfa749655ce32b1ca9f3fe1f9b9ff9f0d6d50c00
pekobeni <arimkim.workspace@gmail.com>
2024-06-27 12:41:54 +0900

    springboot myspring project update 1

---
+++ src/main/java/com/ex01/myspring/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.ex01.myspring.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class MainController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        log.info("******** MainController hello!");
+        return "hello";
+    }
+
+}
